8d1394da79b4b7185600df53c65d5c550c0f63ad
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-03-10 14:41:38 +0100

    Added auto keyword to return type of the methods

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -14,13 +14,14 @@ namespace Complex {
   template<typename T>
   struct Complex {
       Complex(T const& a, T const& b) : real(a), imag(b) {}
+      Complex() = default;
 
-      T real;
-      T imag;
+      T real = 0;
+      T imag = 0;
   };
 
   template<typename T>
-  void square( Complex<T>& a ) {
+  auto square( Complex<T>& a ) {
     // (a+bi)(a+bi) = a2 + 2abi - b2
     const T temp = a.imag;
     a.imag = 2 * a.imag * a.real;
@@ -28,14 +29,14 @@ namespace Complex {
   }
 
   template<typename T>
-  Complex<T>& operator+=( Complex<T>& a, Complex<T> const& b ) {
+  auto& operator+=( Complex<T>& a, Complex<T> const& b ) {
       a.real += b.real;
       a.imag += b.imag;
       return a;
   }
 
   template<typename T>
-  T norm2(Complex<T> const& z) {
+  auto norm2(Complex<T> const& z) {
     return z.imag*z.imag + z.real*z.real;
   }
 }
