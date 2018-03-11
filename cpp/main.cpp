f0a623011a319d5be3971feedd29a8d8b7f72e9f
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-03-11 14:47:54 +0100

    Style fix

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -15,7 +15,7 @@ namespace Complex {
       T real{0};
       T imag{0};
 
-      auto& operator+=( const Complex<T> &b ) {
+      auto& operator+=(const Complex<T> &b) {
           real += b.real;
           imag += b.imag;
           return *this;
@@ -23,7 +23,7 @@ namespace Complex {
   };
 
   template<typename T>
-  void square( Complex<T>& a ) {
+  void square(Complex<T> &a) {
     // (a+bi)(a+bi) = a2 + 2abi - b2
     const T temp = a.imag;
     a.imag = 2 * a.imag * a.real;
@@ -32,7 +32,7 @@ namespace Complex {
 
 
   template<typename T>
-  auto norm2(Complex<T> const& z) {
+  auto norm2(const Complex<T> &z) {
     return z.imag*z.imag + z.real*z.real;
   }
 }
