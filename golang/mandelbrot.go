7a29f7361b7e39becb4884b988368c4d1b1bab1c
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-08-09 19:52:48 +0200

    Remove useless code

--- golang/mandelbrot.go
+++ golang/mandelbrot.go
@@ -17,55 +17,50 @@ const (
 	chunkSize      int = 512
 )
 
-type comp struct {
-	real float64
-	imag float64
-}
+func main() {
 
-func (c *comp) add(c2 *comp) {
-	c.real += c2.real
-	c.imag += c2.imag
-}
+	start := time.Now()
 
-func (c *comp) square() {
-	c.real, c.imag = c.real*c.real-c.imag*c.imag, 2*c.real*c.imag
-}
-
-func (c *comp) norm2() float64 {
-	return c.real*c.real + c.imag*c.imag
-}
-
-func mandelbrot(x, y int) bool {
-	const (
-		halfWidth  = float64(width) / 2
-		halfHeight = float64(height) / 2
-	)
-
-	a := complex(0, 0)
-	c := complex((float64(x)-halfWidth)/halfHeight*2, (float64(y)-halfHeight)/halfHeight*2)
-
-	for i := 0; i < iterationCount; i++ {
-		//a = a*a + c
-		a *= a
-		a += c
-
-		if real(a)*real(a)+imag(a)*imag(a) >= 4 {
-			return false
+	// fan-out
+	taskChan := make(chan task, 8)
+	go func() {
+		for i := 0; i < int(math.Ceil(float64(width*height)/float64(chunkSize))); i++ {
+			end := (i + 1) * chunkSize
+			if end >= width*height {
+				end = width * height
+			}
+			taskChan <- &chunkOfPixelsTask{i * chunkSize, end, width}
 		}
+		close(taskChan)
+	}()
+
+	// workers
+	fmt.Printf("Starting %d workers\n", runtime.NumCPU())
+	resultChan := make(chan *pixel, 8)
+	defer close(resultChan)
+	go func() {
+		for i := 0; i < runtime.NumCPU(); i++ {
+			go worker(resultChan, taskChan)
+		}
+	}()
+
+	// drawer
+	img := image.NewGray(image.Rect(0, 0, width, height))
+	for i := 0; i < width*height; i++ {
+		p := <-resultChan
+		img.Pix[p.pos.y*img.Stride+p.pos.x] = p.value
 	}
 
-	return true
+	elapsed := time.Since(start)
+	fmt.Printf("%s\n", elapsed)
 
-}
+	outputFile, err := os.Create("mandelbrot.png")
+	defer outputFile.Close()
+	if err != nil {
+		panic(err)
+	}
 
-type pixel struct {
-	value uint8
-	pos   position
-}
-
-type position struct {
-	x int
-	y int
+	png.Encode(outputFile, img)
 }
 
 type task interface {
@@ -112,48 +107,34 @@ func worker(resultChan chan *pixel, taskChan chan task) {
 	}
 }
 
-func main() {
+func mandelbrot(x, y int) bool {
+	const (
+		halfWidth  = float64(width) / 2
+		halfHeight = float64(height) / 2
+	)
 
-	start := time.Now()
+	a := complex(0, 0)
+	c := complex((float64(x)-halfWidth)/halfHeight*2, (float64(y)-halfHeight)/halfHeight*2)
 
-	// fan-out
-	taskChan := make(chan task, 8)
-	go func() {
-		for i := 0; i < int(math.Ceil(float64(width*height)/float64(chunkSize))); i++ {
-			end := (i + 1) * chunkSize
-			if end >= width*height {
-				end = width * height
-			}
-			taskChan <- &chunkOfPixelsTask{i * chunkSize, end, width}
+	for i := 0; i < iterationCount; i++ {
+		//a = a*a + c
+		a *= a
+		a += c
+
+		if real(a)*real(a)+imag(a)*imag(a) >= 4 {
+			return false
 		}
-		close(taskChan)
-	}()
-
-	// workers
-	fmt.Printf("Starting %d workers\n", runtime.NumCPU())
-	resultChan := make(chan *pixel, 8)
-	defer close(resultChan)
-	go func() {
-		for i := 0; i < runtime.NumCPU(); i++ {
-			go worker(resultChan, taskChan)
-		}
-	}()
-
-	// drawer
-	img := image.NewGray(image.Rect(0, 0, width, height))
-	for i := 0; i < width*height; i++ {
-		p := <-resultChan
-		img.Pix[p.pos.y*img.Stride+p.pos.x] = p.value
 	}
 
-	elapsed := time.Since(start)
-	fmt.Printf("%s\n", elapsed)
+	return true
+}
 
-	outputFile, err := os.Create("mandelbrot.png")
-	defer outputFile.Close()
-	if err != nil {
-		panic(err)
-	}
+type pixel struct {
+	value uint8
+	pos   position
+}
 
-	png.Encode(outputFile, img)
+type position struct {
+	x int
+	y int
 }
